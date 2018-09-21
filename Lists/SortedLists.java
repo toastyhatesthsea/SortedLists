0b7207dd65db2f39797ef7eaf88b256f4371beec
Saena <toastyhatesthesea@gmail.com>
2018-09-21 04:57:07 -0700

    Added Logic to mergeTwoLists

--- Lists/SortedLists.java
+++ Lists/SortedLists.java
@@ -7,6 +7,33 @@ public class SortedLists
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2)
     {
+        ListNode answer = new ListNode(0);
+        ListNode current = l1;
+
+
+        while (current != null)
+        {
+            int l1Value = current.val;
+            int l2Value = l2.val;
+
+            ListNode l2Link = l2.next;
+
+            if (l1Value >= l2Value)
+            {
+                ListNode aNewValue = new ListNode(l2Value);
+                aNewValue.next = current;
+                l2 = l2.next;
+            }
+            else
+            {
+                ListNode l2NextNode = current.next;
+                ListNode aNewValue = new ListNode(l2Value);
+                current.next = aNewValue;
+                aNewValue.next = l2NextNode;
+            }
+
+        }
+
         return null;
     }
 
